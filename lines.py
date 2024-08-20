22d26d56767d31b124c2c77f6949a66fa6000912
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-20 14:35:37 +0200

    Add files via upload

--- lines.py
+++ lines.py
@@ -1,11 +1,26 @@
 import sys
 def main():
+    length=
     #check for length of sys.argv and respond accordingly
     if sys.argv < 2:
         sys.exit("Too few command-line arguments")
     elif sys.argv > 2:
         sys.exit
+    else:
+        with open(f"{sys.argv[1]}") as file:
+            lines = file.readlines()
+    for line in lines:
+        if line.lstrip().startswith("#"):
+            comment+= lines(line)
+        elif line
+
+        else:
+            length.append(line)
 
 
 
-    with open(sys.argv[1])
+
+
+main()
+
+
