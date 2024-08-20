225f561a90a7a23df640aadea8be7323d27ec824
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-20 14:19:59 +0200

    Add files via upload

---
+++ lines.py
@@ -0,0 +1,11 @@
+import sys
+def main():
+    #check for length of sys.argv and respond accordingly
+    if sys.argv < 2:
+        sys.exit("Too few command-line arguments")
+    elif sys.argv > 2:
+        sys.exit
+
+
+
+    with open(sys.argv[1])
