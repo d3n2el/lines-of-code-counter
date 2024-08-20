f8077b81e9779ae19bf27c9111be4acc657aef81
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-20 14:48:41 +0200

    Add files via upload

--- lines.py
+++ lines.py
@@ -1,19 +1,20 @@
 import sys
 def main():
-    length=
+    length=[]
+    comment = ""
     #check for length of sys.argv and respond accordingly
-    if sys.argv < 2:
+    if len(sys.argv) < 2:
         sys.exit("Too few command-line arguments")
-    elif sys.argv > 2:
+    elif len(sys.argv) > 2:
         sys.exit
+    if not sys.argv[1].endswith(".py"):
+        sys.exit("Not a Python file")
     else:
         with open(f"{sys.argv[1]}") as file:
             lines = file.readlines()
     for line in lines:
-        if line.lstrip().startswith("#"):
-            comment+= lines(line)
-        elif line
-
+        if line.lstrip().startswith("#") or line.strip() == "":
+            continue
         else:
             length.append(line)
 
